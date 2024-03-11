3b4c15cbe82d996a651914bcd6a144836fa63b55
Nyasente <4984ramock@gmail.com>
2024-03-11 12:11:00 -0700

    3 task solved

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -3,4 +3,4 @@
 print- is a fuction
 return: returns a string on success
 """
-print("\"Programming is like building a multilingual puzzle \n")
+print("\"Programming is like building a multilingual puzzle\n")
