f7098c3700fbdd72116713c693bdc010b094ea96
Nyasente <4984ramock@gmail.com>
2024-03-13 03:13:03 -0700

    concat

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-str = "object-oriented programming with Python"
-print(str)
+str = ['Python', 'is', 'an', 'interpreted', 'interactive', 'object-oriented', 'programming'
+ 'language', 'that', 'combines', 'remarkable', 'power', 'with', 'very', 'clear', 'syntax']
+
+print(str[5]  + ' ' + str[6] + ' ' + str[-4] + ' ' + str[0])
