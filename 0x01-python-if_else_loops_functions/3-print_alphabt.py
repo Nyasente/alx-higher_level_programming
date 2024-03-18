f9d669fe7e59c91892e282c60821c3191ef54cba
Nyasente <4984ramock@gmail.com>
2024-03-18 02:11:38 -0700

    ascii

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -4,7 +4,7 @@ print- retrns all letters except q,e
 for: loop iterates over every word
 if : loop to check for q,e imposter
 """
-for letters in range(ord('a'), ord('z') + 1):
-    if letters in (ord('q'), ord('e')):
+for letters in range (97,123):
+    if letters in [101, 113]:
         continue
     print("{}".format(chr(letters)), end='')
