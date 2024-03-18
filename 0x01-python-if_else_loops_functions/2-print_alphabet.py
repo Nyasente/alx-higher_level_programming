ef48c0c28bbea4d10b2f77a80cca62f9b8c275a9
Nyasente <4984ramock@gmail.com>
2024-03-17 23:33:42 -0700

    letters

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -4,4 +4,4 @@ Prints alphabet letters in lowercase.
 For: A loop to print all the ASCII characters in lowercase.
 '''
 for char in range(97, 123):
-    print(f"{chr(char)}", end='')
+    print(chr(char), end='')
