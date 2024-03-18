4cc8a46ac0fbe7ff2d4d6ba9b06515574b98dc40
Nyasente <4984ramock@gmail.com>
2024-03-17 19:44:40 -0700

    ascii

---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''
+print-returns alphabet letters in lower case
+for: a loop to print all the ASCII characters in lower case
+'''
+for alphabets in range(97, 123):
+    print(chr(alphabets), end='')
